a668a1d44a750247df294df86d5ce0f90827a8ef
Danila <shevcovdanila7@gmail.com>
2022-04-25 14:36:16 +0300

    edit jest tests

--- main.spec.ts
+++ main.spec.ts
@@ -11,3 +11,19 @@ it('test of spacesRemove2', ()=>{
 it('calculate test_1',()=>{
     expect(calculate('1')).toBe('1')
 })
+
+it('calculate test_2',()=>{
+    expect(calculate('5 + (25 - 15) * 3 - (7 * 3)')).toBe('24')
+})
+
+it('calculate test_3',()=>{
+    expect(calculate('(1 + 2 * 3 * 9 -21 / 6)')).toBe('10')
+})
+
+it('calculate test_4',()=>{
+    expect(calculate('((3 + 3 * 4) - (9 - 21))')).toBe('36')
+})
+
+it('calculate test_5',()=>{
+    expect(calculate('((((160 / 2) / 4) / 5) * 4)')).toBe('16')
+})
