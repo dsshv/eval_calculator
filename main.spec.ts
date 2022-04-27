48b9b1e7d83c1f7f23798b4629fa7676a249ab8c
Danila <shevcovdanila7@gmail.com>
2022-04-27 14:06:11 +0300

    edit logics of function

--- main.spec.ts
+++ main.spec.ts
@@ -1,29 +1,28 @@
-import {spacesRemove} from "./main";
-import {calculate} from "./main";
+import {evalFunc} from "./main";
 
-it('test of spacesRemove', ()=>{
-    expect(spacesRemove('hgagfd fasfaw eawe   sad w ae  wawe aw d sa d asd')).toBe('hgagfdfasfaweawesadwaewaweawdsadasd')
-})
-it('test of spacesRemove2', ()=>{
-    expect(spacesRemove('a b c')).toBe('abc')
-})
+describe('tets_main', () =>{
 
-it('calculate test_1',()=>{
-    expect(calculate('1')).toBe('1')
-})
+    it('calculate test_1',()=>{
+        expect(evalFunc('1')).toBe('1')
+    })
 
-it('calculate test_2',()=>{
-    expect(calculate('5 + (25 - 15) * 3 - (7 * 3)')).toBe('24')
-})
+    it('calculate test_2',()=>{
+        expect(evalFunc('5 + (25 - 15) * 3 - (7 * 3)')).toBe('24')
+    })
 
-it('calculate test_3',()=>{
-    expect(calculate('(1 + 2 * 3 * 9 -21 / 6)')).toBe('10')
-})
+    it('calculate test_3',()=>{
+        expect(evalFunc('(1 + 2 * 3 * 9 -21 / 6)')).toBe('10')
+    })
 
-it('calculate test_4',()=>{
-    expect(calculate('((3 + 3 * 4) - (9 - 21))')).toBe('36')
-})
+    it('calculate test_4',()=>{
+        expect(evalFunc('((3 + 3 * 4) - (9 - 21))')).toBe('36')
+    })
 
-it('calculate test_5',()=>{
-    expect(calculate('((((160 / 2) / 4) / 5) * 4)')).toBe('16')
+    it('calculate test_5',()=>{
+        expect(evalFunc('((((160 / 2) / 4) / 5) * 4)')).toBe('16')
+    })
+
+    it('calculate test_6',()=>{
+        expect(evalFunc('asdasdas')).toBe(`String "asdasdas" contains invalid symbols`)
+    })
 })
--- main.ts
+++ main.ts
@@ -1,24 +1,9 @@
-export function spacesRemove(str: string){
-    let newStr: string = '';
-    for (let i = 0; i < str.length; i++){
-        if (str[i] == ' ')
-            continue;
-        newStr += str[i];
-    }
-    return newStr;
-}
+export function evalFunc(mathExpression: string){
 
-function checkStringIsValid(checkedString: string){
-    const ALLOWED_SYMBOLS: string = '1234567890-+*/() ';
-    for (let currentChar of checkedString){
-        if (ALLOWED_SYMBOLS.indexOf(currentChar) == -1) {
-            return false;
-        }
-    }
-    return true;
-}
+    mathExpression = spacesRemove(mathExpression);
+    if (checkStringIsValid(mathExpression) == false)
+        return `String "${mathExpression}" contains invalid symbols`
 
-function evalFunc(mathExpression: string){
     while((mathExpression.indexOf("(") != -1) && (mathExpression.indexOf(")") != -1)) {
         let right: number = mathExpression.indexOf(')');
         let left: number = mathExpression.lastIndexOf('(', right) + 1;
@@ -28,39 +13,52 @@ function evalFunc(mathExpression: string){
     }
     mathExpression = parseString(mathExpression)
     return mathExpression
-}
 
-function parseString(newStr: string){
-    while (parseNumber(newStr).length != newStr.length) {
-        let argA: string = parseNumber(newStr);
-        let argB: string = parseNumber(newStr.substring(argA.length + 1));
-        let mathOperation: string = newStr[argA.length];
-        let result = (mathOperation == '-') ? () => Number(argA) - Number(argB) :
-            (mathOperation == '+') ? () => Number(argA) + Number(argB) :
-                (mathOperation == '*') ? () => Number(argA) * Number(argB) :
-                    () => Number(argA) / Number(argB);
-        newStr = String(result()) + newStr.substring(argA.length + argB.length + 1);
+    function spacesRemove(str: string){
+        let newStr: string = '';
+        for (let i = 0; i < str.length; i++){
+            if (str[i] == ' ')
+                continue;
+            newStr += str[i];
+        }
+        return newStr;
     }
-    return newStr
-}
-
-function parseNumber(str: string){
-    const NUMBERS: string = '1234567890';
-    let parsedNumber: string = str[0];
-    if ((parsedNumber == '/') || (parsedNumber == '*')) return 'Error. Number cannot start with "/" or "*"';
-    for (let i = 1; i < str.length; i++){
-        if (NUMBERS.indexOf(str[i]) != -1){
-            parsedNumber += str[i];
-        } else break;
+    function checkStringIsValid(checkedString: string){
+        const ALLOWED_SYMBOLS: string = '1234567890-+*/() ';
+        for (let currentChar of checkedString){
+            if (ALLOWED_SYMBOLS.indexOf(currentChar) == -1) {
+                return false;
+            }
+        }
+        return true;
     }
-    return parsedNumber
+
+    function parseString(newStr: string){
+        while (parseNumber(newStr).length != newStr.length) {
+            let argA: string = parseNumber(newStr);
+            let argB: string = parseNumber(newStr.substring(argA.length + 1));
+            let mathOperation: string = newStr[argA.length];
+            let result = (mathOperation == '-') ? () => Number(argA) - Number(argB) :
+                (mathOperation == '+') ? () => Number(argA) + Number(argB) :
+                    (mathOperation == '*') ? () => Number(argA) * Number(argB) :
+                        () => Number(argA) / Number(argB);
+            newStr = String(result()) + newStr.substring(argA.length + argB.length + 1);
+        }
+        return newStr
+
+        function parseNumber(str: string){
+            const NUMBERS: string = '1234567890';
+            let parsedNumber: string = str[0];
+            if ((parsedNumber == '/') || (parsedNumber == '*')) return 'Error. Number cannot start with "/" or "*"';
+            for (let i = 1; i < str.length; i++){
+                if (NUMBERS.indexOf(str[i]) != -1){
+                    parsedNumber += str[i];
+                } else break;
+            }
+            return parsedNumber
+        }
+    }
 }
 
-export function calculate(str: string){
-    if (checkStringIsValid(str) == false) return `String "${str}" contains invalid symbols`;
-    str = spacesRemove(str);
-    return evalFunc(str);
-}
-
-console.log(calculate('5 + (25 - 15) * 3 - (7 * 3)'))
+console.log(evalFunc('5 + (25 - 15) * 3 - (7 * 3)'))
 
